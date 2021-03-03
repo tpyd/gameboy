972cd097d8399e413f72e2b0f132deb173072965
tpyd <112725276+tpyd@users.noreply.github.com>
2021-03-03 21:06:34 +0100

    Added CPU with ADD instruction fully implemented

--- src/main.rs
+++ src/main.rs
@@ -96,6 +96,93 @@ impl Registers {
     }
 }
 
+/*
+    All CPU instructions for LR35902
+*/
+enum Instruction {
+    ADD(ArithmeticTarget),
+    ADDHL(ArithmeticTarget),
+    SUB(ArithmeticTarget),
+    SBC,
+    AND,
+    OR,
+    XOR,
+    CP,
+    INC,
+    DEC,
+    CCF,
+    SCF,
+    RRA,
+    RLA,
+    RRCA,
+    RRLA,
+    CPL,
+    BIT,
+    RESET,
+    SET,
+    SRL,
+    RR,
+    RL,
+    RRC,
+    RLC,
+    SRA,
+    SLA,
+    SWAP,
+}
+
+enum ArithmeticTarget {
+    A, B, C, D, E, H, L,
+}
+
+/*
+    Implementation of the CPU LR35902
+*/
+struct CPU {
+    registers: Registers,
+}
+
+impl CPU {
+    fn execute(&mut self, instruction: Instruction) {
+        match instruction {
+            Instruction::ADD(target) => { self.add(target) }
+            _ => {} // TODO add more instructions
+        }
+    }
+
+    /*
+        ADD instruction.
+        Reads the current value from target register. Adds the value to register A overflowing if necessary.
+        Updates the flags register and writer the new value to register A.
+    */
+    fn add(&mut self, target: ArithmeticTarget) {
+        let value: u8;
+        match target {
+            ArithmeticTarget::A => { let value = self.registers.a; },
+            ArithmeticTarget::B => { let value = self.registers.b; },
+            ArithmeticTarget::C => { let value = self.registers.c; },
+            ArithmeticTarget::D => { let value = self.registers.d; },
+            ArithmeticTarget::E => { let value = self.registers.e; },
+            ArithmeticTarget::H => { let value = self.registers.h; },
+            ArithmeticTarget::L => { let value = self.registers.l; },
+        }
+        let (new_value, did_overflow) = self.registers.a.overflowing_add(value);
+        /*
+            Flags have to be set according to the following rules:
+            Zero: true if the result of the operation is 0
+            Subtract: true if the operation was a subtraction
+            Carry: true if operation resulted in overflow
+            Half Carry: true if the operation resulted in overflow from lower nibble to upper nibble
+        */
+        self.registers.f.zero = new_value == 0;
+        self.registers.f.subtract = false;
+        self.registers.f.carry = did_overflow;
+        // Check if half carry needs to be set by adding both lower nibbles together and see if the result is greater than 1111 (0xF)
+        self.registers.f.half_carry = (self.registers.a & 0xF) + (value & 0xF) > 0xF;
+
+        self.registers.a = new_value
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
