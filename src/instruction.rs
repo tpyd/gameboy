80953707907a42657ccfa15b9c31fa656431016a
tpyd <112725276+tpyd@users.noreply.github.com>
2022-11-27 10:26:12 +0100

    Added more display implementations for instructions

--- src/instruction.rs
+++ src/instruction.rs
@@ -716,8 +716,10 @@ impl fmt::Display for Instruction {
             &Instruction::INC(arithmetic_type) => {
                 write!(f, "INC    {}", arithmetic_type.to_string())?;
                 Ok(())
-            }
-            _ => write!(f, "test"),
+            },
+            &Instruction::RET(jump_condition) => write!(f, "RET    {}", jump_condition),
+            &Instruction::NOP => write!(f, "NOP"),
+            _ => write!(f, "not implemented"),
         }
     }
 }
@@ -733,16 +735,58 @@ impl Display for LoadType {
 impl Display for ArithmeticType {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            ArithmeticType::Byte(byte_target) => {
-                match byte_target {
-                    ByteTarget::A => {
-                        write!(f, "A")?;
-                        Ok(())
-                    },
-                    _ => Ok(()),
-                }
-            },
-            _ => Ok(()),
+            ArithmeticType::Byte(byte_target) => write!(f, "{}", byte_target),
+            &ArithmeticType::Word(word_target) => write!(f, "{}", word_target),
+            &ArithmeticType::SP => write!(f, "SP"),
+        }
+    }
+}
+
+
+impl Display for ByteTarget {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ByteTarget::A => write!(f, "A"),
+            ByteTarget::B => write!(f, "B"),
+            &ByteTarget::C => write!(f, "C"),
+            ByteTarget::D => write!(f, "D"),
+            &ByteTarget::E => write!(f, "E"),
+            ByteTarget::H => write!(f, "H"),
+            &ByteTarget::L => write!(f, "L"),
+            ByteTarget::D8 => write!(f, "D8"),
+            &ByteTarget::HLI => write!(f, "HLI"),
+        }
+    }
+}
+
+impl Display for WordTarget {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            WordTarget::BC => write!(f, "BC"),
+            WordTarget::DE => write!(f, "DE"),
+            &WordTarget::HL => write!(f, "HL"),
+            WordTarget::SP => write!(f, "SP"),
+        }
+    }
+}
+
+impl Display for JumpType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            JumpType::Word(jump_condition) => write!(f, "{}", jump_condition),
+            JumpType::Address => write!(f, "Address"),
+        }
+    }
+}
+
+impl Display for JumpCondition {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            JumpCondition::NotZero => write!(f, "NZ"),
+            JumpCondition::Zero => write!(f, "Z"),
+            JumpCondition::NotCarry => write!(f, "NC"),
+            JumpCondition::Carry => write!(f, "C"),
+            JumpCondition::Always => write!(f, ""),
         }
     }
 }
